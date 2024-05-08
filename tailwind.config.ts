aa391cdb50b1c1e1fdf9f6b6439e90400f5444a2
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-05-08 18:12:04 +0200

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: '#daf5f0',
+        main: '#c4a1ff',
+        mainAccent: '#9e66ff',
+      },
+      borderRadius: {
+        base: '5px',
+      },
+      boxShadow: {
+        base: '4px 4px 0px 0px rgba(0,0,0,1)',
+      },
+      translate: {
+        boxShadowX: '4px',
+        boxShadowY: '4px',
+      },
+      fontWeight: {
+        base: '500',
+        heading: '700',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
