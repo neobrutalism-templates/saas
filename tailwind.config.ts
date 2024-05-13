a50ce73ee9686efb856557a96681fc9f1444a207
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-05-13 23:41:03 +0200

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,21 @@
 import type { Config } from 'tailwindcss'
+import tailwindAnimate from 'tailwindcss-animate'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      width: {
+        container: '1300px',
+      },
       colors: {
-        bg: '#daf5f0',
-        main: '#c4a1ff',
-        mainAccent: '#9e66ff',
+        bg: '#E0E7F1',
+        main: '#A3E636',
       },
       borderRadius: {
         base: '5px',
@@ -24,11 +28,39 @@ const config: Config = {
         boxShadowY: '4px',
       },
       fontWeight: {
-        base: '500',
-        heading: '700',
+        base: '400',
+        heading: '600',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        marquee: 'marquee 15s linear infinite',
+        marquee2: 'marquee2 15s linear infinite',
+      },
+      screens: {
+        w900: { raw: '(max-width: 900px)' },
+        w500: { raw: '(max-width: 500px)' },
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimate],
 }
 export default config
