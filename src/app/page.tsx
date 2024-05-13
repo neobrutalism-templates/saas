eaa03fd9f9ccde419f4ab6bccf4788cf858167f8
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-05-13 23:42:07 +0200

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Community from '@/sections/community'
+import Features from '@/sections/features'
+import Header from '@/sections/header'
+import Faq from '@/sections/faq'
+import Pricing from '@/sections/pricing'
+import Footer from '@/components/footer'
+
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <Header />
+      <Features />
+      <Community />
+      <Faq />
+      <Pricing />
+      <Footer />
+    </>
+  )
 }
