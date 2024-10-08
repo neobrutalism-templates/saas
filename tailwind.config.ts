60baffdef2bea5c9d4c4f52cdd80fac920fe82e8
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-10-08 05:56:18 +0200

    updated variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,18 +16,18 @@ const config: Config = {
       colors: {
         main: '#A3E636',
         mainAccent: '#88cc19', // not needed for shadcn components
-        overlay: 'rgba(0,0,0,0.8)',
-        secondaryBlack: '#1b1b1b', // opposite of plain white, not used pitch black because borders and box-shadows are that color 
-
+        overlay: 'rgba(0,0,0,0.8)', // background color overlay for alert dialogs, modals, etc.
+  
         // light mode
         bg: '#E0E7F1',
         text: '#000',
         border: '#000',
-
+  
         // dark mode
-        darkBg: '#111903',
+        darkBg: '#2c312b',
         darkText: '#eeefe9',
         darkBorder: '#000',
+        secondaryBlack: '#212121', // opposite of plain white, not used pitch black because borders and box-shadows are that color 
       },
       borderRadius: {
         base: '5px',
